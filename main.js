dd1af3b11a272582a8c1c3a5b91921b0b6787a86
Ivan Stickmanov <stickman9000@mail.ru>
2021-04-26 00:31:04 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let main = document.querySelector('#main');
+let resultInput = document.querySelector('#result');
+
+let data;
+let url = '/data.json';
+
+fetch(url)
+    .then(response => response.json())
+    .then(commit => {
+        data = commit;
+    })
+    .then(() => {
+        generateContent();
+});
+
+// generates all the sections
+function generateContent() {
+    main.innerHTML = '';
+    for (let i of data) {
+        main.innerHTML += '<h3>' + i.title + '</h3>';
+
+        let generatedList = '';
+
+        for (let j of i.list) {
+            if (typeof j == 'string') {
+                generatedList += '<a class="symbol-box">' + j + '</a>';
+            }
+            else {
+                generatedList += '<a class="symbol-box" data-tags"' + j[1] + '">' + j[0] + '</a>';
+            }
+        }
+
+        main.innerHTML += '<section class="flex">' + generatedList + '</section>';
+    }
+
+    for (let k of document.querySelectorAll('.symbol-box')) {
+        k.addEventListener('click', () => {
+            resultInput.value = resultInput.value + k.innerText
+        })
+    }
+}
